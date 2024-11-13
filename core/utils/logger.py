43096af9eab9e8819a7c25c1fe48d793347da882
snowflake1212 <76563139+snowflake1212@users.noreply.github.com>
2024-11-14 01:03:08 +0700

    Update logger.py

--- core/utils/logger.py
+++ core/utils/logger.py
@@ -1,31 +1,25 @@
 import sys
 import re
+import os
 from datetime import date
-
 from loguru import logger
 
-
 def logging_setup():
-
     format_info = "<green>{time:HH:mm:ss.SS}</green> <blue>{level}</blue> <level>{message}</level>"
     format_error = "<green>{time:HH:mm:ss.SS}</green> <blue>{level}</blue> | " \
                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | <level>{message}</level>"
-    file_path = r"logs/"
-    # if sys.platform == "win32":
+    file_path = "logs/"
+    
+    # Ensure log directory exists
+    os.makedirs(file_path, exist_ok=True)
 
     logger.remove()
-
-    logger.add(file_path + f"out_{date.today().strftime('%m-%d')}.log", colorize=True,
-               format=format_info)
-
-    logger.add(sys.stdout, colorize=True,
-               format=format_info, level="INFO")
-
+    logger.add(file_path + f"out_{date.today().strftime('%m-%d')}.log", colorize=False, format=format_info)
+    logger.add(sys.stdout, colorize=True, format=format_info, level="INFO")
 
 def clean_brackets(raw_str):
     clean_text = re.sub(brackets_regex, '', raw_str)
     return clean_text
-
 
 brackets_regex = re.compile(r'<.*?>')
 
