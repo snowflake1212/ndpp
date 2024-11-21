dfcd8fa3dbc79dbb7f8cd682fd790bd7c9604493
snowflake1212 <76563139+snowflake1212@users.noreply.github.com>
2024-11-21 23:55:44 +0700

    Create 24run.sh

---
+++ 24run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Changing nameserver..."
+echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
+
+# Restart Grass Node
+pkill -f rvpdn.py &&
+echo "Killing openvpn..."
+killall openvpn
+sleep 5
+rm -f rvpdn.py &&
+wget https://raw.githubusercontent.com/snowflake1212/ndpp/refs/heads/main/rvpdn.py &&
+chmod +x rvpdn.py
+python3 random_gtvpn.py JP KR US RO TH VN BR MY QA RU ZZ ID AR &
+sleep 20
+
+echo "Running grass.py..."
+pkill -f npbot.py
+python3 npbot.py &
