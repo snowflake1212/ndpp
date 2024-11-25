7c78072bb4a701c89d35b5d011e39ca67e20e147
snowflake1212 <76563139+snowflake1212@users.noreply.github.com>
2024-11-25 21:31:57 +0700

    Update 24run.sh

--- 24run.sh
+++ 24run.sh
@@ -10,9 +10,9 @@ sleep 5
 rm -f rvpdn.py &&
 wget https://raw.githubusercontent.com/snowflake1212/ndpp/refs/heads/main/rvpdn.py &&
 chmod +x rvpdn.py
-python3 random_gtvpn.py JP KR &
+python3 rvpdn.py JP KR &
 sleep 20
 
 echo "Running grass.py..."
-#pkill -f npbot.py
-#python3 npbot.py &
+pkill -f npbot.py
+python3 npbot.py &
